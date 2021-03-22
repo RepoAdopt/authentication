48e4457ed2a71d391fc3d246529bb0ec3b843b9e
BTaapken <beau@lioncode.nl>
2021-03-22 15:20:25 +0100

    * Github authentication

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
 from os import getenv
+
+from lib.github import authenticate
 
 load_dotenv()
 
 
 app = Flask(__name__)
 CORS(app)
+
+
+@app.route("/signin", methods=['POST'])
+def signin():
+    client_id = request.args.get('client_id')
+    code = request.args.get('code')
+    return authenticate(client_id, code)
 
 
 if __name__ == '__main__':
