12ffbaae7391f1d06e4ec3026106b4270a11387c
Niek125 <niek.sleddens@gmail.com>
2021-03-24 15:14:05 +0100

    create tokens

--- lib/github.py
+++ lib/github.py
@@ -2,6 +2,7 @@ from requests import post, get
 from os import getenv
 from jwcrypto import jwt, jwk
 import urllib.parse
+import time
 
 
 def authenticate(client_id, code):
@@ -18,9 +19,20 @@ def authenticate(client_id, code):
                         headers={'Authorization': 'token ' + oauth_json['access_token'][0]})
     user = user_response.json()
 
+    now = int(time.time())
     key = jwk.JWK(generate='oct', size=256)
-    token = jwt.JWT(header={'alg': 'HS256'}, claims={
-                    'user_id': user['id'], 'username': user['login'], 'email': user['email']})
+    token = jwt.JWT(header={'alg': 'HS256'},
+                    claims={
+                        'user_id': user['id'],
+                        'username': user['login'],
+                        'email': user['email']},
+                    default_claims={
+                        'jti': True,
+                        'iss': getenv('ISSUER'),
+                        'aud': getenv('ISSUER'),
+                        'nbf': now,
+                        'iat': now,
+                        'exp': now + getenv('TOKEN_EXPIRATION_TIME')})
     token.make_signed_token(key)
     print(token.serialize())
 
