7441c02ac38f8fdd6efb05991d87ad39d1275367
Niek125 <niek.sleddens@gmail.com>
2021-04-20 11:04:53 +0200

    establish gateway connection

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import sleep
+from requests import post
 from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -11,6 +13,41 @@ from lib.github import authenticate
 app = Flask(__name__)
 CORS(app)
 
+MAX_RETRIES = 20
+
+
+def establish_gateway_connection(attempts=0):
+    base_url = f'http://{getenv("GATEWAY")}/services'
+    name = "authentication"
+    try:
+        res = post(
+            url=base_url,
+            data={"name": name, "url": getenv("GRAPHQL_URL")},
+        )
+
+        if res.status_code == 201:
+            post(
+                url=f"{base_url}/{name}/routes",
+                data={"name": "signin", "paths[]": "/signin"},
+            )
+
+            print("Created gateway connection!")
+
+            return
+        elif res.status_code == 409:
+            print("Gateway connection already created!")
+            
+            return
+        else:
+            print("Could not create gateway connection!")
+
+    except:
+        print("Gateway is not available!")
+
+    sleep(0.2)
+
+    if attempts <= MAX_RETRIES:
+        establish_gateway_connection(attempts=attempts + 1)
 
 @app.route("/signin", methods=["POST"])
 def signin():
@@ -20,4 +57,6 @@ def signin():
 
 
 if __name__ == "__main__":
+    establish_gateway_connection()
+
     app.run(debug=getenv("DEVELOPMENT"), host="0.0.0.0", port=getenv("PORT"))
